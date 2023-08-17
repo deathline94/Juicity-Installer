176bd8292b5de9dea1d895af0e886b3987e66eb2
deathline94 <dlb4all@gmail.com>
2023-08-17 17:24:40 +0330

    Update juicity-installer.sh

--- juicity-installer.sh
+++ juicity-installer.sh
@@ -2,21 +2,24 @@
 
 # ---- Install required packages ----
 sudo apt-get update
-sudo apt-get install -y unzip jq uuid-runtime
+sudo apt-get install -y unzip curl jq uuid-runtime openssl
+
+# ---- Define directories and URLs ----
+DOWNLOAD_DIR="/root/juicity"
+DOWNLOAD_LOCATION="$DOWNLOAD_DIR/juicity.zip"
+LATEST_RELEASE_URL="https://github.com/juicity/juicity/releases/download/v0.1.3/juicity-linux-x86_64.zip"
+
+# ---- Create the download directory ----
+mkdir -p "$DOWNLOAD_DIR"
 
 # ---- Download the binary ----
-LATEST_RELEASE_URL="https://github.com/juicity/juicity/releases/download/v0.1.3/juicity-linux-x86_64.zip"
-curl -L "$LATEST_RELEASE_URL" -o "/root/juicity/juicity.zip"
+curl -L "$LATEST_RELEASE_URL" -o "$DOWNLOAD_LOCATION"
 
 # ---- Extract the binary ----
-mkdir -p /root/juicity
-unzip /root/juicity/juicity.zip -d /root/juicity
+unzip "$DOWNLOAD_LOCATION" -d "$DOWNLOAD_DIR"
 
-# ---- Cleanup: Delete all files except juicity-server ----
-find /root/juicity ! -name 'juicity-server' -type f -exec rm -f {} +
-
-# ---- Set permissions ----
-chmod +x /root/juicity/juicity-server
+# ---- Set permissions for the server binary ----
+chmod +x "$DOWNLOAD_DIR/juicity-server"
 
 # ---- Generate config file ----
 read -p "Enter listen port (or press enter to randomize): " PORT
@@ -25,18 +28,18 @@ read -p "Enter password: " PASSWORD
 UUID=$(uuidgen)
 
 # Generate private key and certificate
-openssl ecparam -genkey -name prime256v1 -out /root/juicity/private.key
-openssl req -new -x509 -days 36500 -key /root/juicity/private.key -out /root/juicity/fullchain.cer -subj "/CN=bing.com"
+openssl ecparam -genkey -name prime256v1 -out "$DOWNLOAD_DIR/private.key"
+openssl req -new -x509 -days 36500 -key "$DOWNLOAD_DIR/private.key" -out "$DOWNLOAD_DIR/fullchain.cer" -subj "/CN=bing.com"
 
 # Create config.json
-cat > /root/juicity/config.json <<EOL
+cat > "$DOWNLOAD_DIR/config.json" <<EOL
 {
   "listen": ":$PORT",
   "users": {
     "$UUID": "$PASSWORD"
   },
-  "certificate": "/root/juicity/fullchain.cer",
-  "private_key": "/root/juicity/private.key",
+  "certificate": "$DOWNLOAD_DIR/fullchain.cer",
+  "private_key": "$DOWNLOAD_DIR/private.key",
   "congestion_control": "bbr",
   "log_level": "info"
 }
@@ -52,9 +55,9 @@ After=network.target nss-lookup.target
 [Service]
 Type=simple
 User=root
-ExecStart=/root/juicity/./juicity-server run -c /root/juicity/config.json
-StandardOutput=file:/root/juicity/juicity-server.log
-StandardError=file:/root/juicity/juicity-server.log
+ExecStart=$DOWNLOAD_DIR/juicity-server run -c $DOWNLOAD_DIR/config.json
+StandardOutput=file:$DOWNLOAD_DIR/juicity-server.log
+StandardError=file:$DOWNLOAD_DIR/juicity-server.log
 Restart=on-failure
 LimitNPROC=512
 LimitNOFILE=infinity
@@ -69,5 +72,5 @@ sudo systemctl enable juicity
 sudo systemctl start juicity
 
 # ---- Generate and print the share link ----
-SHARE_LINK=$(/root/juicity/./juicity-server generate-sharelink -c /root/juicity/config.json)
+SHARE_LINK=$($DOWNLOAD_DIR/juicity-server generate-sharelink -c $DOWNLOAD_DIR/config.json)
 echo "Share Link: $SHARE_LINK"
