3ceffa8f1ab96ea6410d62fdaa57c29c885841df
deathline94 <dlb4all@gmail.com>
2023-08-23 17:07:44 +0330

    v1-reworked everything , added menu , clean up etc

--- juicity-installer.sh
+++ juicity-installer.sh
@@ -1,123 +1,142 @@
 #!/bin/bash
 
+# Variables
+INSTALL_DIR="/root/juicity"
+CONFIG_FILE="$INSTALL_DIR/config.json"
+SERVICE_FILE="/etc/systemd/system/juicity.service"
+JUICITY_SERVER="$INSTALL_DIR/juicity-server"
+
 # Function to print characters with delay
 print_with_delay() {
-    text=$1
-    delay=$2
+    text="$1"
+    delay="$2"
     for ((i = 0; i < ${#text}; i++)); do
         echo -n "${text:$i:1}"
         sleep $delay
     done
+    echo
 }
 
 # Introduction animation
-echo ""
-echo ""
-print_with_delay "j" 0.1
-print_with_delay "u" 0.1
-print_with_delay "i" 0.1
-print_with_delay "c" 0.1
-print_with_delay "i" 0.1
-print_with_delay "t" 0.1
-print_with_delay "y" 0.1
-print_with_delay "-" 0.1
-print_with_delay "i" 0.1
-print_with_delay "n" 0.1
-print_with_delay "s" 0.1
-print_with_delay "t" 0.1
-print_with_delay "a" 0.1
-print_with_delay "l" 0.1
-print_with_delay "l" 0.1
-print_with_delay "e" 0.1
-print_with_delay "r" 0.1
-print_with_delay " " 0.1
-print_with_delay "b" 0.1
-print_with_delay "y" 0.1
-print_with_delay " " 0.1
-print_with_delay "D" 0.1
-print_with_delay "E" 0.1
-print_with_delay "A" 0.1
-print_with_delay "T" 0.1
-print_with_delay "H" 0.1
-print_with_delay "L" 0.1
-print_with_delay "I" 0.1
-print_with_delay "N" 0.1
-print_with_delay "E" 0.1
-print_with_delay " " 0.1
-print_with_delay "|" 0.1
-print_with_delay " " 0.1
-print_with_delay "n" 0.1
-print_with_delay "a" 0.1
-print_with_delay "m" 0.1
-print_with_delay "e" 0.1
-print_with_delay "l" 0.1
-print_with_delay "e" 0.1
-print_with_delay "s" 0.1
-print_with_delay " " 0.1
-print_with_delay "g" 0.1
-print_with_delay "h" 0.1
-print_with_delay "o" 0.1
-print_with_delay "u" 0.1
-print_with_delay "l" 0.1
-print_with_delay ""
-echo ""
-echo ""
-
-#!/bin/bash
+print_with_delay "juicity-installer by DEATHLINE | @NamelessGhoul" 0.1
 
 # Install required packages
-sudo apt-get update
-sudo apt-get install -y unzip jq uuid-runtime
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install -y unzip jq uuid-runtime > /dev/null 2>&1
 
-# Detect OS and download the corresponding release
-OS=$(uname -s)
-if [ "$OS" == "Linux" ]; then
-    BINARY_NAME="juicity-linux-x86_64.zip"
-else
-    echo "Unsupported OS: $OS"
-    exit 1
+# Check for an existing installation
+if [[ -d $INSTALL_DIR && -f $SERVICE_FILE ]]; then
+    echo "Juicity already installed. Choose an option:"
+    echo "1. Reinstall"
+    echo "2. Modify"
+    echo "3. Uninstall"
+    read -p "Enter your choice (1/2/3): " choice
+
+    case $choice in
+        1)
+            echo "Reinstalling..."
+            sudo systemctl stop juicity
+            sudo systemctl disable juicity > /dev/null 2>&1
+            rm -rf $INSTALL_DIR
+            rm -f $SERVICE_FILE
+            ;;
+        2)
+            read -p "Enter new listen port: " PORT
+            sed -i "s/\"listen\": \":.*\"/\"listen\": \":$PORT\"/" $CONFIG_FILE
+            sudo systemctl restart juicity
+            SHARE_LINK=$($JUICITY_SERVER generate-sharelink -c $CONFIG_FILE)
+            echo "New Share Link: $SHARE_LINK"
+            exit 0
+            ;;
+        3)
+            sudo systemctl stop juicity
+            sudo systemctl disable juicity > /dev/null 2>&1
+            rm -rf $INSTALL_DIR
+            rm -f $SERVICE_FILE
+            echo "Uninstalled successfully!"
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice!"
+            exit 1
+            ;;
+    esac
 fi
 
-LATEST_RELEASE_URL=$(curl --silent "https://api.github.com/repos/juicity/juicity/releases" | jq -r '.[0].assets[] | select(.name == "'$BINARY_NAME'") | .browser_download_url')
+# Detect Architecture
+ARCH=$(uname -m)
+BINARY_NAME="juicity-linux"
 
-# Download and extract to /root/juicity
-mkdir -p /root/juicity
-curl -L $LATEST_RELEASE_URL -o /root/juicity/juicity.zip
-unzip /root/juicity/juicity.zip -d /root/juicity
+case "$ARCH" in
+    "x86_64")
+        BINARY_NAME+="-x86_64.zip"
+        ;;
+    "arm64")
+        BINARY_NAME+="-arm64.zip"
+        ;;
+    "armv7")
+        BINARY_NAME+="-armv7.zip"
+        ;;
+    "mips32")
+        BINARY_NAME+="-mips32.zip"
+        ;;
+    "mips64")
+        BINARY_NAME+="-mips64.zip"
+        ;;
+    "riscv64")
+        BINARY_NAME+="-riscv64.zip"
+        ;;
+    "i686")
+        BINARY_NAME+="-x86_32.zip"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+LATEST_RELEASE_URL=$(curl --silent "https://api.github.com/repos/juicity/juicity/releases" | jq -r ".[0].assets[] | select(.name == \"$BINARY_NAME\") | .browser_download_url")
+
+# Download and extract
+mkdir -p $INSTALL_DIR
+curl -sL $LATEST_RELEASE_URL -o "$INSTALL_DIR/juicity.zip"
+unzip -q "$INSTALL_DIR/juicity.zip" -d $INSTALL_DIR
 
 # Delete all files except juicity-server
-find /root/juicity ! -name 'juicity-server' -type f -exec rm -f {} +
+find $INSTALL_DIR ! -name 'juicity-server' -type f -exec rm -f {} +
 
 # Set permissions
-chmod +x /root/juicity/juicity-server
+chmod +x $JUICITY_SERVER
 
 # Create config.json
-read -p "Enter listen port (or press enter to randomize): " PORT
-[[ -z "$PORT" ]] && PORT=$((RANDOM % 65500 + 1))
-read -p "Enter password: " PASSWORD
+read -p "Enter listen port (or press enter to randomize between 10000 and 65535): " PORT
+[[ -z "$PORT" ]] && PORT=$((RANDOM % 55536 + 10000))
+read -p "Enter password (or press enter to generate one): " PASSWORD
+if [[ -z "$PASSWORD" ]]; then
+    PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%^&*()' | fold -w 8 | head -n 1)
+    echo "Generated Password: $PASSWORD"
+fi
 UUID=$(uuidgen)
 
-# Generate private key
-openssl ecparam -genkey -name prime256v1 -out /root/juicity/private.key
+# Generate keys
+openssl ecparam -genkey -name prime256v1 -out "$INSTALL_DIR/private.key"
+openssl req -new -x509 -days 36500 -key "$INSTALL_DIR/private.key" -out "$INSTALL_DIR/fullchain.cer" -subj "/CN=www.speedtest.net"
 
-# Generate certificate using the private key
-openssl req -new -x509 -days 36500 -key /root/juicity/private.key -out /root/juicity/fullchain.cer -subj "/CN=www.speedtest.net"
-
-cat > /root/juicity/config.json <<EOL
+cat > $CONFIG_FILE <<EOL
 {
   "listen": ":$PORT",
   "users": {
     "$UUID": "$PASSWORD"
   },
-  "certificate": "/root/juicity/fullchain.cer",
-  "private_key": "/root/juicity/private.key",
+  "certificate": "$INSTALL_DIR/fullchain.cer",
+  "private_key": "$INSTALL_DIR/private.key",
   "congestion_control": "bbr",
   "log_level": "info"
 }
 EOL
 
 # Create systemd service file
-cat > /etc/systemd/system/juicity.service <<EOL
+cat > $SERVICE_FILE <<EOL
 [Unit]
 Description=juicity-server Service
 Documentation=https://github.com/juicity/juicity
@@ -126,9 +145,9 @@ After=network.target nss-lookup.target
 [Service]
 Type=simple
 User=root
-ExecStart=/root/juicity/./juicity-server run -c /root/juicity/config.json
-StandardOutput=file:/root/juicity/juicity-server.log
-StandardError=file:/root/juicity/juicity-server.log
+ExecStart=$JUICITY_SERVER run -c $CONFIG_FILE
+StandardOutput=file:$INSTALL_DIR/juicity-server.log
+StandardError=file:$INSTALL_DIR/juicity-server.log
 Restart=on-failure
 LimitNPROC=512
 LimitNOFILE=infinity
@@ -139,10 +158,9 @@ EOL
 
 # Reload systemd, enable and start the service
 sudo systemctl daemon-reload
-sudo systemctl enable juicity
+sudo systemctl enable juicity > /dev/null 2>&1
 sudo systemctl start juicity
-sudo systemctl restart juicity
 
-# Generate and print the share link
-SHARE_LINK=$(/root/juicity/./juicity-server generate-sharelink -c /root/juicity/config.json)
+# Modified share link output
+SHARE_LINK=$($JUICITY_SERVER generate-sharelink -c $CONFIG_FILE)
 echo "Share Link: $SHARE_LINK"
